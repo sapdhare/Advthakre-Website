1d0cc68511958ccae1ff225875728531a9d4eac6
Śãüřàbh Šâpđhàře <50585218+sapdhare@users.noreply.github.com>
2026-06-10 17:54:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 # Database Connection
 conn = pyodbc.connect(
-       f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+    f"DRIVER={{ODBC Driver 18 for SQL Server}};"
     f"SERVER={os.getenv('DB_SERVER')};"
     f"DATABASE={os.getenv('DB_DATABASE')};"
     f"UID={os.getenv('DB_USERNAME')};"
     f"PWD={os.getenv('DB_PASSWORD')};"
+    f"Encrypt=no;"
     f"TrustServerCertificate=yes;"
 )
 
